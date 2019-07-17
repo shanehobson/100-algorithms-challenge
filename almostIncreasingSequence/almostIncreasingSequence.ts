a6632a72385f731f438c34f5ec22a34d1cb542d6
Shane Hobson <shanehobson1@gmail.com>
2019-07-16 21:31:43 -0700

    Completed more algorithms

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,10 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
-
+    let mistakes = 0;
+    for (let i = 0; i < sequence.length - 1 && mistakes < 2; i++) {
+        if (sequence[i] >= sequence[i + 1]) mistakes++;
+    }
+    return mistakes < 2;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+console.log(almostIncreasingSequence([1, 3, 2])) 
--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,10 @@
 function alphabeticShift(inputString: string): string {
-
+    for (let i = 0; i < inputString.length; i++) {
+        const charCode = inputString.charCodeAt(i);
+        const newChar = inputString[i] === 'z' ? 'a' : String.fromCharCode(charCode + 1);
+        inputString = inputString.slice(0, i) + newChar + inputString.slice(i + 1);
+    }
+    return inputString;
 }
 
 console.log(alphabeticShift('crazy'));
